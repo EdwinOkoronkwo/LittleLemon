ee63ee45a026eb3e1ac0f38769a280e39ffe0e7f
Edwin Okoronkwo <49468467+EdwinOkoronkwo@users.noreply.github.com>
2023-08-27 12:15:17 -0600

    Added Tests

--- littlelemon/restaurant/tests.py
+++ littlelemon/restaurant/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from restaurant.models import Menu
+from .serializers import MenuSerializer
 
-# Create your tests here.
+
+class MenuTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(title="IceCream", price=80, inventory=100)
+        self.assertEqual(item.title, "IceCream")
+        self.assertEqual(item.price, 80)
+        self.assertEqual(item.inventory, 100)
+        
+        
+# class MenuViewTest(TestCase):
+#     def setUp(self) -> None:
+        
+#         self.bread = Menu.objects.create(title='Bread', price=4.99, inventory=20)
+#         self.pancake = Menu.objects.create(title='Pan Cake', price=12.99, inventory=50)
+        
+#     def test_getall(self):    
+#         menu = Menu.objects.all()
+#         instances = [
+#         {'id': 1, 'title': 'Bread', 'price': 4.99, 'inventory': 20},
+#        {'id': 2, 'title': 'Pan Cake', 'price': 12.99, 'inventory': 50},
+#     ]
+#         serializer = MenuSerializer(menu, many=True)
+#         self.assertEqual(serializer, instances)
+
+        
+
